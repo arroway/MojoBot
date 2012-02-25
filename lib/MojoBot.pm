dcc36b89485e06acb5daa8159e6df58341c3b2c3
arroway <stephanie@minet.net>
2012-02-25 19:18:43 +0100

    display data from the database in the public page

--- lib/MojoBot.pm
+++ lib/MojoBot.pm
@@ -1,6 +1,7 @@
 package MojoBot;
 use Mojo::Base 'Mojolicious';
 use MyUsers;
+use DBI;
 
 
 sub startup {
@@ -17,6 +18,7 @@ sub startup {
   my $r = $self->routes;
 
   $r->any('/')->to('display#index')->name('index');
+  #$r->get('db_connect')->to('display#db_connect')->name('db_connect');
   $r->any('/login')->to('login#login')->name('login');
   $r->get('/protected')->to('login#protected')->name('protected');
   $r->get('/logout')->to('login#logout')->name('logout')
